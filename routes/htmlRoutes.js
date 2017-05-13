0c6bc782c9e2fb0a893470812b99289b858cac17
Chris Manley <mrchristophermanley@gmail.com>
2017-05-13 12:49:47 -0400

    routes accomplished

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,20 +1,14 @@
 // *********************************************************************************
-// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// htmlRoutes.js - this file offers a set of routes for sending users to the various html pages
 // *********************************************************************************
 
 // Dependencies
 // =============================================================
+var homeController = require('../controllers/home');
 var path = require("path");
 
 // Routes
 // =============================================================
 module.exports = function(app) {
-
-  // Each of the below routes just handles the HTML page that the user gets sent to.
-
-  // index route loads view.html
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname + "/buyorbuck/views/layouts/main.handlebars"));
-  });
-
+  app.get('/', homeController.renderHome);
 };
--- server.js
+++ server.js
@@ -1,44 +1,42 @@
-// *****************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
-// *** Dependencies
-// =============================================================
-var express = require("express");
-var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
-
-// Sets up the Express App
-// =============================================================
+//App dependencies -----------------------------------------/
+var express = require('express');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var methodOverride = require('method-override');
 var app = express();
+
+
+//App middleware -------------------------------------------/
+app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+app.use(express.static(process.cwd() + "/public"));
+
+//Handlebars config ---------------------------------------/
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+//Route config -------------------------------------------/
+require('./routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
+
+//Database config ---------------------------------------/
+global.db = require('./models');
+
+//Port config ---------------------------------------------------/
 var PORT = process.env.PORT || 3000;
 
-// Requiring our models for syncing
-var db = require("./models");
-
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
-// Static directory
-app.use(express.static("/views"));
-
-// Set Handlebars.
-var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-// Routes =============================================================
-
-// require("./routes/html-routes.js")(app);
-// require("./routes/api-routes.js")(app);
-
-// Syncing our sequelize models and then starting our express app
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+//Starting the server, syncing our models ------------------------------------/
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function(err) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.info("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+    }
   });
 });
