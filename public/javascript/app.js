cc46d4aeb88894d49caee0924d1a645cf05fff7c
Leslie Moses <LesMoses@gmail.com>
2017-05-16 16:54:52 -0400

    design updates, static start

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -9,3 +9,16 @@ $('[data-app-dashboard-toggle-shrink]').on('click', function(e) {
 $('.sim-thumb').on('click', function() {
     $('#main-product-image').attr('src', $(this).data('image'));
 })
+
+// designer's profile card from designLogin
+// more click
+$('.card-profile-stats-more-link').click(function(e){
+  e.preventDefault();
+  if ( $(".card-profile-stats-more-content").is(':hidden') ) {
+    $('.card-profile-stats-more-link').find('i').removeClass('fa-angle-down').addClass('fa-angle-up');
+  } else {
+    $('.card-profile-stats-more-link').find('i').removeClass('fa-angle-up').addClass('fa-angle-down');
+  }
+  $(this).next('.card-profile-stats-more-content').slideToggle();
+});
+
