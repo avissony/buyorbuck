03ba9fc119c3198a5ca649380d9b8421a575d631
Chris Manley <mrchristophermanley@gmail.com>
2017-05-18 08:30:07 -0400

    finishing signup page and post logic

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO Designer (name, email, password, focus, bio)
+VALUES ("Vernon Devyn", "vernon.devyn@gmail.com", "Pasw0rd", "maleclothing", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.");
+
+INSERT INTO Designer (name, email, password, focus, bio)
+VALUES ("Carver Carol","carver.carol@gmail.com", "Pasw0rd", "femaleclothing", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.");
+
+INSERT INTO Designer (name, email, password, focus, bio)
+VALUES ("Elly Kimberley", "ellyKim@gmail.com", "Pasw0rd", "femaleclothing", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.");
+
+INSERT INTO Designer (name, email, password, focus, bio)
+VALUES ("Lorinda Shelby", "lorinda.shelby@gmail.com", "Pasw0rd", "childrensclothing", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.");
+
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,5 +12,25 @@ var db = require("../models");
 // Routes
 // =============================================================
 module.exports = function(app) {
+
+  // GET route for testing	
   app.get('/test', testApiController.index);
+
+  // GET route for getting all of the designers
+  app.get("/api/designer", function(req, res) {
+    var query = {};
+    db.Designer.findAll({
+      where: query
+    }).then(function(dbDesigner) {
+      res.json(dbDesigner);
+    });
+  });
+
+  // POST route for saving a new post
+  app.post("/api/designer", function(req, res) {
+    db.Designer.create(req.body).then(function(dbPost) {
+      res.json(dbDesigner);
+    });
+  });
+
 };
